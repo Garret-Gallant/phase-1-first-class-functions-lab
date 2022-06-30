2a11c6a83161e50e9b7c0b44ef4074b315736af2
Garret Gallant <gallanttechnological@gmail.com>
2022-06-30 15:50:21 -0600

    lesson complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(array){
+    return array.slice(0 , 2);
+}
+
+function returnLastTwoDrivers(array){
+    return array.slice(2 , 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiple){
+    return function fareMultiplier(fare){
+        return fare * multiple;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (array, selectingDrivers) => {
+    return selectingDrivers(array);
+};
+
+
+
+
+
+
+
