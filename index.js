e7f5e275ba7091cca91bd533dbfd01c275e094a9
Garret Gallant <gallanttechnological@gmail.com>
2022-07-01 17:07:58 -0600

    commit tree 56644705daf808c51bcbf15ad18ea491c5f0da84

--- index.js
+++ index.js
@@ -18,12 +18,40 @@ function createFareMultiplier(multiple){
 
 const fareDoubler = createFareMultiplier(2);
 
-
 const fareTripler = createFareMultiplier(3);
 
 const selectDifferentDrivers = (array, selectingDrivers) => {
     return selectingDrivers(array);
 };
+
+// // Code your solution in this file!
+
+// const drivers = ['Antonia','Nuru','Amari','Mo']
+// const returnFirstTwoDrivers = function(drivers){
+//  returnFirstTwoDrivers(drivers)
+// //  return drivers.slice(0,2)
+// }
+
+// const returnLastTwoDrivers = function(drivers){
+//     return drivers.slice(2,4)
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+// const createFareMultiplier = function(int){
+//  return function(int){
+//    return int * int
+//  }
+// }
+
+// const fareDoubler = function(int){
+//    return (createFareMultiplier(2)) 
+// }
+
+
+
+
+
 
 
 
